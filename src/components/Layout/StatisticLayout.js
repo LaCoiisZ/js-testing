21c23e5c6a95164f75447e4785987b84d47c4b85
Anawach <anawach.c@geotalent.co.th>
2018-06-26 14:53:44 +0700

    tesing Highcharts

--- src/components/Layout/StatisticLayout.js
+++ src/components/Layout/StatisticLayout.js
@@ -1,9 +1,39 @@
 import React, { PureComponent } from "react";
+import Highcharts from "highcharts";
 
 class StatisticSection extends PureComponent {
+
+    componentDidMount() {
+        const myChart = Highcharts.chart('chart-container', {
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Fruit Consumption'
+            },
+            xAxis: {
+                categories: ['Apples', 'Bananas', 'Oranges']
+            },
+            yAxis: {
+                title: {
+                    text: 'Fruit eaten'
+                }
+            },
+            series: [{
+                name: 'Jane',
+                data: [1, 0, 4]
+            }, {
+                name: 'John',
+                data: [5, 7, 3]
+            }]
+        });
+    }
+
     render() {
         return (
-            <section className="section-statistic"></section>
+            <section className="section-statistic">
+                <div id="chart-container" style={{ width : "100%", height: "100%"}}></div>
+            </section>
         );
     }
 }
